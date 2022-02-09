cdeb735519979edf73bc286a74ee693a16187153
joeyhusky <76567879+joeyhusky@users.noreply.github.com>
2022-02-08 21:25:49 -0800

    add computeGuess

--- src/word-utils.ts
+++ src/word-utils.ts
@@ -4,3 +4,23 @@ export function getRandomWord() {
   const randomIndex = Math.floor(Math.random() * wordbank.length);
   return wordbank[randomIndex];
 }
+
+export enum LetterState {
+  Miss,
+  Present,
+  Match,
+}
+
+export const computeGuess = (guess: string, wordStr: string): LetterState[] => {
+  const result: LetterState[] = [];
+
+  const answerArray = wordStr.split("");
+  const guessArray = guess.split("");
+
+  guessArray.forEach((letter, idx) => {
+    if (letter === answerArray[idx]) result.push(LetterState.Match);
+    else if (answerArray.includes(letter)) result.push(LetterState.Present);
+    else result.push(LetterState.Miss);
+  });
+  return result;
+};
