312c3e471f5d1592691f769442b5df4b667bf1f2
joeyhusky <76567879+joeyhusky@users.noreply.github.com>
2022-03-08 22:21:44 -0800

    styling fixes in gameover modal

--- src/GameOverModal.tsx
+++ src/GameOverModal.tsx
@@ -9,21 +9,19 @@ const GameOverModal: React.FC = () => {
   return (
     <div
       role="modal"
-      className="absolute shadow-xl bg-slate-100 border border-gray-400 rounded text-center w-11/12 h-3/4 p-6 left-0 right-0 mx-auto top-24 grid grid-rows-6"
+      className="absolute shadow-xl bg-slate-100 border border-gray-400 rounded text-center flex flex-col space-y-6 w-11/12 p-6 left-0 right-0 mx-auto top-24"
     >
       <b className="text-4xl">
         {hasWon ? "Nice Work Norberdler!" : "Game Over"}
       </b>
       <img src={norb} />
-      <span />
-      <span />
       <WordRow
         letters={answer}
         shouldBreathe={false}
         className="items-center"
       />
       <button
-        className="border border-green-500 mx-4 bg-green-500 rounded mt-4 shadow"
+        className="border border-green-500 mx-4 bg-green-500 rounded w-11/12 h-14 mt-4 shadow"
         onClick={newGame}
       >
         New Game
