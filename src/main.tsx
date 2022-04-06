315961b2242fc094062cd4f59ff5b3c86648eca0
joeyhusky <76567879+joeyhusky@users.noreply.github.com>
2022-04-05 18:20:18 -0700

    add favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Favicon from "react-favicon";
+import icon from "./public/favicon.ico";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url={icon} />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
